fc5caabbcda11b6cd38441b43ccfe71db84975d7
EdenBeavis <ebbeavis@gmail.com>
2021-07-04 23:55:07 +1000

    Added test fixture Added some unit test and data from personal project

---
+++ product.api.test/Fixtures/ProductsTestFixture.cs
@@ -0,0 +1,41 @@
+﻿using product.api.test.Infrastructure;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace product.api.test.Fixtures
+{
+    public class ProductsTestFixture : TestWebApplicationFactory
+    {
+        private const string ProductApi = "api/products";
+
+        public async Task<(HttpResponseMessage message, string stringContent)> GetProduct(string query)
+        {
+            var response = await Client.GetAsync($"{ProductApi}/{query}");
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return (response, responseString);
+        }
+
+        //public async Task<(HttpResponseMessage message, string stringContent)> PostPoduct(string query, Product productDto)
+        //{
+        //    var json = JsonConvert.SerializeObject(productDto);
+        //    var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+        //    var response = await Client.PostAsync($"{ProductApi}/{query}", stringContent);
+        //    var responseString = await response.Content.ReadAsStringAsync();
+
+        //    return (response, responseString);
+        //}
+
+        //public async Task<(HttpResponseMessage message, string stringContent)> UpdateProduct(string query, Product productDto)
+        //{
+        //    var json = JsonConvert.SerializeObject(productDto);
+        //    var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+        //    var response = await Client.PatchAsync($"{ProductApi}/{query}", stringContent);
+        //    var responseString = await response.Content.ReadAsStringAsync();
+
+        //    return (response, responseString);
+        //}
+    }
+}
--- product.api.test/Tests/UnitTest1.cs
+++ product.api.test/Tests/UnitTest1.cs
@@ -1,14 +1,56 @@
-using System;
 using Xunit;
 
 namespace product.api.test
 {
-    public class UnitTest1
+    public class UnitTest1 : IClassFixture<OrdersTestFixture>
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly OrdersTestFixture _fixture;
 
+        public OrdersUnitTest(OrdersTestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        #region When an order is created and is being retireved by Id
+
+        [Fact]
+        public async Task ThenReturnOrderWithId()
+        {
+            _fixture.RefDataProvider.SetupRefDataForOrders();
+
+            // Arrange
+            var id = 5;
+            await _fixture.CreateSingleDefaultOrder(id);
+
+            // Act
+            var (message, stringContent) = await _fixture.GetOrders(id.ToString());
+
+            //Assert
+            var expected = new OrderDto().WithDefault().WithId(5);
+            var response = JsonConvert.DeserializeObject<OrderDto>(stringContent, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Objects });
+
+            message.EnsureSuccessStatusCode();
+            response.Should().NotBeNull();
+            response.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [ClassData(typeof(OrdersGetTest))]
+        public async Task ThenReturnOrdersWithQueryedStatusOrId(OrdersGetTestData testData)
+        {
+            _fixture.RefDataProvider.SetupRefDataForOrders();
+
+            // Arrange
+            await _fixture.CreateOrders(testData.Setup);
+
+            // Act
+            var (message, stringContent) = await _fixture.GetOrders(testData.Query);
+
+            //Assert
+            var response = JsonConvert.DeserializeObject<IEnumerable<OrderDto>>(stringContent, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Objects });
+
+            message.EnsureSuccessStatusCode();
+            response.Should().BeEquivalentTo(testData.Expected);
         }
     }
 }
